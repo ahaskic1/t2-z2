6efd395891cc9a87237235ced4e3a555373961bf
ahaskic1 <ahaskic1@eth.unsa.ba>
2018-11-10 16:47:23 +0100

    Dodavanje metoda intersect, preklapanje metoda toString i equals
    Zavrsen zadatak

--- src/ba/unsa/etf/rpr/tutorijal02/Interval.java
+++ src/ba/unsa/etf/rpr/tutorijal02/Interval.java
@@ -1,6 +1,6 @@
 package ba.unsa.etf.rpr.tutorijal02;
 
-public class Interval {
+public class Interval extends Object {
     private double pocetna_tacka, krajnja_tacka;
     private boolean ukljucena_prva, ukljucena_druga;
 
@@ -48,10 +48,64 @@ public class Interval {
         return false;
     }
     public boolean isIn(double tacka) {
-        if((this.getPocetna_tacka() < tacka || (this.getPocetna_tacka() == tacka && this.isUkljucena_prva()== true)) && (this.getKrajnja_tacka() > tacka || (this.getKrajnja_tacka() == tacka && this.isUkljucena_druga() == true)))
+        if((this.getPocetna_tacka() < tacka || (this.getPocetna_tacka() == tacka && this.isUkljucena_prva() == true)) && (this.getKrajnja_tacka() > tacka || (this.getKrajnja_tacka() == tacka && this.isUkljucena_druga() == true)))
             return true;
         return false;
     }
-
-
+    public Interval intersect(Interval i) {
+        Interval vrati = new Interval();
+        if(this.getPocetna_tacka() >= i.getPocetna_tacka() && this.getPocetna_tacka() <= i.getKrajnja_tacka()) {
+            if(this.isUkljucena_prva() == true)
+                vrati.setUkljucena_prva(true);
+            else
+                vrati.setUkljucena_prva(false);
+            vrati.setPocetna_tacka(this.getPocetna_tacka());
+        }
+        else if(this.getPocetna_tacka() <= i.getPocetna_tacka() && this.getKrajnja_tacka() >= i.getPocetna_tacka()) {
+            if(i.isUkljucena_prva() == true)
+                vrati.setUkljucena_prva(true);
+            else
+                vrati.setUkljucena_prva(false);
+            vrati.setPocetna_tacka(i.getPocetna_tacka());
+        }
+        if(this.getKrajnja_tacka() <= i.getKrajnja_tacka() && this.getKrajnja_tacka() >= i.getPocetna_tacka()) {
+            if(this.isUkljucena_druga() == true)
+                vrati.setUkljucena_druga(true);
+            else
+                vrati.setUkljucena_druga(false);
+            vrati.setKrajnja_tacka(this.getKrajnja_tacka());
+        }
+        else if(this.getKrajnja_tacka() >= i.getKrajnja_tacka() && this.getPocetna_tacka() <= i.getKrajnja_tacka()) {
+            if(i.isUkljucena_druga() == true)
+                vrati.setUkljucena_druga(true);
+            else
+                vrati.setUkljucena_druga(false);
+            vrati.setKrajnja_tacka(i.getKrajnja_tacka());
+        }
+        return vrati;
+    }
+    public String toString() {
+        if(this.isNull()) {
+            return "()";
+        }
+        String s = new String("");
+        if(this.isUkljucena_prva())
+            s+='[';
+        else
+            s+='(';
+        s+=Double.toString(this.getPocetna_tacka()) + ',' + Double.toString(this.getKrajnja_tacka());
+        if(this.isUkljucena_druga())
+            s+=']';
+        else
+            s+=')';
+        return s;
+    }
+    public static Interval intersect(Interval i1, Interval i2) {
+        return i1.intersect(i2);
+    }
+    public boolean equals(Interval i) {
+        if(this.getPocetna_tacka() == i.getPocetna_tacka() && this.getKrajnja_tacka() == i.getKrajnja_tacka() && this.isUkljucena_prva() == i.isUkljucena_prva() && this.isUkljucena_druga() == i.isUkljucena_druga())
+            return true;
+        return false;
+    }
 }
--- src/ba/unsa/etf/rpr/tutorijal02/Main.java
+++ src/ba/unsa/etf/rpr/tutorijal02/Main.java
@@ -3,6 +3,18 @@ package ba.unsa.etf.rpr.tutorijal02;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Nema ništa u mainu, pokrenite testove!");
+        /*Interval interval = new Interval(0,0,false,false);
+        interval.setPocetna_tacka(1.1);
+        interval.setUkljucena_prva(true);
+        interval.setKrajnja_tacka(5.1);
+        interval.setUkljucena_druga(false);
+
+
+        System.out.println(interval.getPocetna_tacka());
+        System.out.println(interval.isUkljucena_prva());
+        System.out.println(interval.getKrajnja_tacka());
+        System.out.println(interval.isUkljucena_druga());
+        System.out.println(interval.isIn(2));*/
+
     }
 }
