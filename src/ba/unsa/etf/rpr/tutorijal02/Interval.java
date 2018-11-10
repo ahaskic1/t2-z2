409cefae5ac94af7c62fcf835b3e206149080952
ahaskic1 <ahaskic1@eth.unsa.ba>
2018-11-10 14:42:49 +0100

    Dodavanje settera (u prethodnom primjeru nisu bili dodani)

--- src/ba/unsa/etf/rpr/tutorijal02/Interval.java
+++ src/ba/unsa/etf/rpr/tutorijal02/Interval.java
@@ -29,6 +29,18 @@ public class Interval {
     public boolean isUkljucena_druga() {
         return this.ukljucena_druga;
     }
+    public void setPocetna_tacka(double pocetna_tacka) {
+        this.pocetna_tacka = pocetna_tacka;
+    }
+    public void setKrajnja_tacka(double krajnja_tacka) {
+        this.krajnja_tacka = krajnja_tacka;
+    }
+    public void setUkljucena_prva(boolean ukljucena_prva) {
+        this.ukljucena_prva = ukljucena_prva;
+    }
+    public void setUkljucena_druga(boolean ukljucena_druga) {
+        this.ukljucena_druga = ukljucena_druga;
+    }
     public boolean isNull() {
         if(this.getPocetna_tacka() == 0 && this.getKrajnja_tacka() == 0 && this.isUkljucena_prva() == false && this.isUkljucena_druga() == false) {
             return true;
@@ -36,7 +48,7 @@ public class Interval {
         return false;
     }
     public boolean isIn(double tacka) {
-        if((this.getPocetna_tacka() < tacka || (this.getPocetna_tacka() == tacka && this.isUkljucena_prva()==true)) && (this.getKrajnja_tacka()> tacka || (this.getKrajnja_tacka() == tacka && this.isUkljucena_druga()==true)))
+        if((this.getPocetna_tacka() < tacka || (this.getPocetna_tacka() == tacka && this.isUkljucena_prva()== true)) && (this.getKrajnja_tacka() > tacka || (this.getKrajnja_tacka() == tacka && this.isUkljucena_druga() == true)))
             return true;
         return false;
     }
