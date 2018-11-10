aa6b7ba2fdd13e1811bdaf701c1630d475a67f7b
ahaskic1 <ahaskic1@eth.unsa.ba>
2018-11-10 14:16:38 +0100

    Dodavanje konstruktora i njjihova implementacija

--- src/ba/unsa/etf/rpr/tutorijal02/Interval.java
+++ src/ba/unsa/etf/rpr/tutorijal02/Interval.java
@@ -1,6 +1,22 @@
 package ba.unsa.etf.rpr.tutorijal02;
 
 public class Interval {
+    double pocetna_tacka, krajnja_tacka;
+    boolean ukljucena_prva, ukljucena_druga;
+
+    public Interval(double pocetna_tacka, double krajnja_tacka, boolean ukljucena_prva, boolean ukljucena_druga) {
+        if(pocetna_tacka > krajnja_tacka)   throw new IllegalArgumentException("Nemoguc interval");
+        this.pocetna_tacka = pocetna_tacka;
+        this.krajnja_tacka = krajnja_tacka;
+        this.ukljucena_prva = ukljucena_prva;
+        this.ukljucena_druga = ukljucena_druga;
+    }
+    public Interval() {
+        this.pocetna_tacka = 0;
+        this.krajnja_tacka = 0;
+        this.ukljucena_prva = false;
+        this.ukljucena_druga = false;
+    }
 
 
 }
